3458448970d244bb689a7b6b046fe5929a1d1dd4
Reto Spoerri <rspoerri@nouser.org>
2016-08-23 14:34:26 +0200

    make mic prefab active added localhost name for opusnetworkedtest

--- Assets/OpusNetworked.cs
+++ Assets/OpusNetworked.cs
@@ -3,7 +3,17 @@ using System.Collections;
 using UnityEngine.Networking;
 
 public class OpusNetworked : NetworkBehaviour {
-    public string name = "pc";
+    public string name = "something";
+
+    public void Start () {
+        if (isLocalPlayer) {
+            name = System.Environment.MachineName;
+            Debug.Log ("OpusNetworked.Start: local name " + name);
+        } else {
+            Debug.Log ("OpusNetworked.Start: non local name " + name);
+        }
+
+    }
 
     public override bool OnSerialize(NetworkWriter writer, bool initialState) {
         base.OnSerialize(writer, initialState);
@@ -17,7 +27,7 @@ public class OpusNetworked : NetworkBehaviour {
         base.OnDeserialize(reader, initialState);
 
         if (!isLocalPlayer) {
-            name = reader.ReadString();
+            name = reader.ReadString() + "-nonlocal";
         }
     }
 }
