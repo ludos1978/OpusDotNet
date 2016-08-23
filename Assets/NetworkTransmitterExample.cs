324b76ac0f59cf3e7ccd6e3f146b1e235b6f3569
Reto Spoerri - DesktopPC <spoerri@ludic.ch>
2016-08-23 14:09:35 +0200

    net test
    testing networking functions

---
+++ Assets/NetworkTransmitterExample.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkTransmitterExample : NetworkBehaviour {
+     /*//inside a class, derived from network behaviour, which has the NetworkTransmitter component attached...
+     ...
+     //on client and server
+     NetworkTransmitter networkTransmitter = GetComponent<NetworkTransmitter>();
+     ...
+     //on client: listen for and handle received data
+     networkTransmitter.OnDataCompletelyReceived += MyCompletelyReceivedHandler;
+     networkTransmitter.OnDataFragmentReceived += MyFragmentReceivedHandler;
+ 
+     ...
+
+     //on server: transmit data. myDataToSend is an object serialized to byte array.
+     StartCoroutine(networkTransmitter.SendBytesToClientsRoutine(0, myDataToSend))
+     ...
+     //on client this will be called once the complete data array has been received
+     [Client]
+    private void MyCompletelyReceivedHandler(int transmissionId, byte[] data) {
+        //deserialize data to object and do further things with it...
+    }
+    //on clients this will be called every time a chunk (fragment of complete data) has been received
+    [Client]
+    private void MyFragmentReceivedHandler(int transmissionId, byte[] data) {
+        //update a progress bar or do something else with the information
+    }*/
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/OpusNetworked.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class OpusNetworked : NetworkBehaviour {
+    public string name = "pc";
+
+    public override bool OnSerialize(NetworkWriter writer, bool initialState) {
+        base.OnSerialize(writer, initialState);
+
+        writer.Write(name);
+
+        return true;
+    }
+
+    public override void OnDeserialize(NetworkReader reader, bool initialState) {
+        base.OnDeserialize(reader, initialState);
+
+        if (!isLocalPlayer) {
+            name = reader.ReadString();
+        }
+    }
+}
